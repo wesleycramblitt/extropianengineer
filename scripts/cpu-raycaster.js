133b000137ca028541e8b8c9a791b06ef61a52f7
Wesley Cramblitt <wcramblitt@gmail.com>
2026-04-08 10:48:00 -0600

    remove jeckyll from github pages build

--- scripts/cpu-raycaster.js
+++ scripts/cpu-raycaster.js
@@ -193,23 +193,8 @@ document.addEventListener("DOMContentLoaded",async () => {
         "screen_vertices": []
     }
 
-    var mesh = await loadOBJ("../mesh/cat.obj");
-
-
-
-    // console.log(mesh);
-    // var cat = {
-    // "vertices" : mesh.positions, 
-    // "indices": mesh.indices, 
-    //     "T" : T(0,-55,-250),
-    //     "R" : R([0,0, 0], 0),
-    //     "S" : S(0.5,0.5,0.5),
-    //     "world_vertices": [],
-    //     "screen_vertices": []
-    // }
-    
+   var mesh = await loadOBJ("../mesh/cat.obj");
    var cat = await loadJson("../mesh/cat.json"); 
-    
 
     function updateCircularPosition(object) {
         const r = 1;          // radius
